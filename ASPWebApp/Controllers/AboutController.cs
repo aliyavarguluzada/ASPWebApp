239633e577ca75dae604334563b9001b3cac4dc7
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-07-26 13:12:00 +0400

    Add project files.

---
+++ ASPWebApp/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPWebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
